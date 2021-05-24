ab7891c11db528f42bed48f01743dcab897e74d2
Nicole Durand <42581259+nicoledu1234@users.noreply.github.com>
2021-05-23 23:42:46 -0500

    Add files via upload

---
+++ ListaEnlazadaSimple/ListaEnlazadaSimple.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+
+template<class T>
+class asc {
+public:
+	bool operator()(T a, T b) {
+		return a < b;
+	}
+};
+
+
+template<class T>
+class desc {
+public:
+	bool operator()(T a, T b) {
+		return a > b;
+	}
+};
+template<class T>
+class Node {
+public:
+	T value;
+	Node* next;
+	Node(T v) {
+		value = v;
+		next = NULL;
+	}
+};
+
+template<class T, class R>
+class LinkedList {
+	private:
+		Node<T>* head;
+		R r;
+	public:
+		LinkedList() {
+			head = nullptr;
+		}
+		bool Find(T x, Node<T>**& p) {
+			for (p = &head; *p && r((*p)->value,x) ; p = &((*p)->next));
+			return *p && (*p)->value == x;
+		}
+		bool Insert(T x) {
+			Node<T>** p;
+			if (Find(x, p)) {
+				return 0;
+			}
+			Node<T>* n = new Node<T>(x);
+			n->next = *p; 
+			*p=n; 
+			return 1;
+		}
+		bool Remove(T x) {
+			Node<T>** p;
+			if (!Find(x, p)) {
+				return 0; 
+			}
+			Node<T>* t = *p;
+			*p =(*p)->next; 
+			delete(t);
+			return 1;
+		}
+		~LinkedList() {
+			Node<T>** p;
+			for (p = &head; *p ; ) {
+				Node<T>* t = *p;
+				*p = (*p)->next;
+				delete(t);
+			}
+		}
+		void Print() {
+			if (!head) {
+				cout << "Empty Linked List" << endl;
+			}
+			else {
+				for (Node<T>* i = head; i; i = i->next) {
+					cout << i->value << "->";
+				}
+				cout << "|| " << endl;
+			}
+			
+		}
+
+};
+
+
+
+int main() {
+	LinkedList<int,asc<int>> l;
+
+	l.Insert(5);
+	l.Insert(7);
+	l.Insert(9);
+	l.Insert(1);
+	l.Insert(15);
+	l.Insert(3);
+	l.Insert(8);
+	l.Insert(0);
+	l.Insert(25);
+	l.Print();
+	cout << endl;
+	l.Remove(5);
+	l.Print();
+	l.Remove(25);
+	l.Print();
+	l.Remove(0);
+	l.Print();
+	l.~LinkedList();
+	l.Print();
+
+	cout << "Lista Enlazada Simple" << endl;
+	
+}
---
+++ tarea2MergeSort/tarea2MergeSort.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <ctime> 
+
+using namespace std;
+
+template <class T>
+T mod(T t, T m) {
+	T mo = -1;
+	if (t < 0)
+		return (t * mo >= m) ? (t - (t / m * m)) + m : t + m;
+	return (t >= m) ? t - (t / m * m) : t;
+}
+template <class T>
+void printLista(T x[], int n) {
+	cout << "{ ";
+	for (int i = 0; i < n - 1; i++) {
+		cout << x[i] << ",";
+	}
+	cout << x[n - 1] << " } " << endl;
+}
+
+template <class T>
+void merge(T* izq, T nIzq, T* der, T nDer, T* A) {
+	T* k = A;
+	T* i = izq;
+	T* j = der;
+	while ((i < izq + nIzq) && (j < der + nDer)) {
+		if (*i <= *j) {
+			*k = *i;
+			i++;
+		}
+		else {
+			*k = *j;
+			j++;
+		}
+		k++;
+	}
+	while (i < izq + nIzq) {
+		*k = *i;
+		i++; k++;
+	}
+	while (j < der + nDer) {
+		*k = *j;
+		j++; k++;
+	}
+}
+template <class T>
+void mergeSort(T* A, int n) {
+	if (n == 1) {
+		return;
+	}
+	int mitad = n / 2;
+	T* izq = new T[mitad];
+	T* der = new T[n - mitad];
+	T* k = A;
+	for (T* i = izq; i < izq + mitad; i++) {
+		*i = *k;
+		k++;
+	}
+	T* r = A + mitad;
+	for (T* j = der; j < der + (n - mitad); j++) {
+		*j = *r;
+		r++;
+	}
+
+	mergeSort<T>(izq, mitad);
+	mergeSort<T>(der, n - mitad);
+	merge<T>(izq, mitad, der, n - mitad, A);
+}
+int main() {
+	int tam = 4000000;
+	int* lista = new int[tam];
+
+	int aleatorio;
+	for (int* i = lista; i < lista + tam; i++) {
+		aleatorio = mod(rand(), (tam - 0 + 1) + 1);
+		*i = aleatorio;
+	}
+
+	unsigned t0, t1;
+	t0 = clock();
+	mergeSort<int>(lista, tam);
+
+	t1 = clock();
+	double time = (double(t1 - t0) / CLOCKS_PER_SEC);
+	cout << "Execution Time: " << time << endl;
+
+	delete[] lista;
+	return 0;
+}
